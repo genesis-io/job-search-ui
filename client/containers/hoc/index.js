f5e383873e0863abcf1a593a46b009f390926477
MarianoTheMaker <mokpalefe1@gmail.com>
2017-11-06 18:40:11 -0800

    fix naming for hoc

--- client/containers/hoc/index.js
+++ client/containers/hoc/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-export default (ComposedComponent) => {
+export const Authentication = (ComposedComponent) => {
   class Auth extends Component {
     render() {
       return <ComposedComponent { ...this.props } />;
