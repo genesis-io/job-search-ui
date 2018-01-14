974bf3ba5759c49399998574e481da64f79c0125
Mariano Okpalefe <mokpalefe1@gmail.com>
2018-01-14 03:06:13 -0800

    WIP login test

--- client/containers/login/login.test.js
+++ client/containers/login/login.test.js
@@ -4,11 +4,25 @@ import { createMockStore } from 'redux-test-utils';
 import Login from './login';
 import { mountWithStore } from '../../../config/testHelpers';
 
-test('render home component', () => {
+let wrapper;
+
+beforeAll(() => {
   const testState = {
     user: {},
   }
   const store = createMockStore(testState)
-  const wrapper = mountWithStore(<Login location={'/login'} />, store);
+  wrapper = mountWithStore(<Login location={'/login'} />, store);
+})
+
+test('render home component', () => {
   expect(wrapper).toMatchSnapshot();
 });
+
+test('it should render login button if url is jobsearch.com/login', () => {
+  expect(wrapper.find('login-form-sign-in').text()).toEqual('Log In');
+})
+
+// test('it should render register button if url is jobsearch.com/signup', () => {
+//   wrapper = mountWithStore(<Login location={'/signup'} />, store);
+//   expect(wrapper.find('login-form-sign-in').text()).toEqual('Register');
+// })
